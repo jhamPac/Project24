80f371caf81e2e0aa91b6826e9b0db28c3b89f05
Kevia <cloud@kevia.me>
2016-02-12 14:22:25 -0800

    layout done

---
+++ SecureSave/SecureSave/ViewController.swift
@@ -0,0 +1,49 @@
+//
+//  ViewController.swift
+//  SecureSave
+//
+//  Created by jhampac on 2/12/16.
+//  Copyright © 2016 jhampac. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController
+{
+    @IBOutlet weak var secret: UITextView!
+    
+    @IBAction func authenticateUser(sender: UIButton)
+    {
+        
+    }
+    
+
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+        
+        let notificationCenter = NSNotificationCenter.defaultCenter()
+        notificationCenter.addObserver(self, selector: "adjustForKeyboard:", name: UIKeyboardWillHideNotification, object: nil)
+        notificationCenter.addObserver(self, selector: "adjustForKeyboard:", name: UIKeyboardWillChangeFrameNotification, object: nil)
+
+    }
+    
+    func adjustForKeyboard(notification: NSNotification) {
+        let userInfo = notification.userInfo!
+        
+        let keyboardScreenEndFrame = (userInfo[UIKeyboardFrameEndUserInfoKey] as! NSValue).CGRectValue()
+        let keyboardViewEndFrame = view.convertRect(keyboardScreenEndFrame, fromView: view.window)
+        
+        if notification.name == UIKeyboardWillHideNotification {
+            secret.contentInset = UIEdgeInsetsZero
+        } else {
+            secret.contentInset = UIEdgeInsets(top: 0, left: 0, bottom: keyboardViewEndFrame.height, right: 0)
+        }
+        
+        secret.scrollIndicatorInsets = secret.contentInset
+        
+        let selectedRange = secret.selectedRange
+        secret.scrollRangeToVisible(selectedRange)
+    }
+}
+
